006fefdd7e121c417391ea8f8272b2a9baec406d
Fergus <fdmacpherson@gmail.com>
2023-12-27 18:23:45 +1100

    Attempted fix of datetime filtering

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "fergusm"
-version = "0.1.4"
+version = "0.1.5"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/cliniko/Filters.kt
+++ src/main/kotlin/cliniko/Filters.kt
@@ -3,7 +3,9 @@ package cliniko
 import io.ktor.http.*
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
 import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun equals(field : String, value : String) : Parameters {
     return parametersOf("q[]", "$field:=$value")
@@ -22,7 +24,14 @@ fun inRange(field : String, minVal : String?, maxVal : String?) : Parameters {
 fun instantInRange(field: String, minInstant: Instant?, maxInstant : Instant?) : Parameters {
 
     return inRange(field,
-        minVal = minInstant?.toString(),
-        maxVal = maxInstant?.toString(),
+        minVal = minInstant?.toStringNoMillis(),
+        maxVal = maxInstant?.toStringNoMillis(),
     )
+}
+
+fun Instant.toStringNoMillis() : String {
+    // we have to convert to a datetime, then a java datetime, in order to be able to use the java dateformatter class
+    val dt = this.toLocalDateTime(TimeZone.UTC)
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    return formatter.format(dt.toJavaLocalDateTime())
 }
